d1b63aab95e22ea7ad084dd40df6f0e0dc3d291f
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-09-09 12:23:12 -0700

    add working command line interface to take in user inputs and output results.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,51 @@
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-    System.out.println("It's working!");
+    System.out.println("Welcome to Portland Event Planners! Let's get started by getting some info about your event.");
+    System.out.println(" ");
+    System.out.println("-----------------------------");
+    System.out.println(" ");
+
+    System.out.println("How many people will be attending your event?");
+    int userAttendees = Integer.parseInt(scanner.nextLine());
+    System.out.println("-----------------------------");
+    System.out.println(" ");
+
+    System.out.println("Would you like us to provide catering for your event? Please enter either 'full dinner menu', 'snacks', or 'none'");
+    String userFood = scanner.nextLine();
+    System.out.println("-----------------------------");
+    System.out.println(" ");
+
+    System.out.println("Will you need bar service for you event? Please enter 'yes' or 'no'.");
+    String userBar = scanner.nextLine();
+    System.out.println("-----------------------------");
+    System.out.println(" ");
+
+    System.out.println("We also have a band available that could play at your event. Would you like us to book them? Please enter 'yes' or 'no'.");
+    String userBand = scanner.nextLine();
+    System.out.println("-----------------------------");
+    System.out.println(" ");
+
+    Event newEvent = new Event(userAttendees, userFood);
+
+    System.out.println("Great! Here are the details for your event.");
+    System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXX");
+    System.out.println("Total attendees: " + newEvent.getAttendees());
+    System.out.println("Food selection: " + newEvent.getFood());
+    if (userBar.equalsIgnoreCase("yes")) {
+      newEvent.setBarTrue();
+      System.out.println("+ Full service bar");
+    }
+    if (userBand.equalsIgnoreCase("yes")) {
+      newEvent.setBandTrue();
+      System.out.println("+ Live band");
+    }
+    System.out.println("----------------------");
+    newEvent.setCostPerAttendee();
+    System.out.println("Total Cost = " + newEvent.setTotalCost());
 
   }
 }
